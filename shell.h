d3dea5353be54963eb84962bca2a670a1e390329
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-10-10 23:02:37 +0300

    commit

---
+++ shell.h
@@ -0,0 +1,30 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+
+void _printf(const char *message);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
