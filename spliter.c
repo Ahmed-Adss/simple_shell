2bfe912f094abdc6432cbbce000f705a629b94f9
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-10-17 02:08:11 +0300

    modifying

--- spliter.c
+++ spliter.c
@@ -1,41 +1,32 @@
 #include "shell.h"
-
 char **spliter(char *line)
 {
-	char *split = *temp = NULL;
+	char *split = NULL, *temp = NULL;
 	char **cmd = NULL;
-	int i = 0;
+	int i = 0, l = 0;
 
 	if (!line)
-	{
 		return (NULL);
-	}
 	temp = _strdup(line);
 	split = strtok(temp, SPECIFIER);
-
 	if (split == NULL)
 	{
 		free(line);
 		free(temp);
 		return (NULL);
 	}
-
 	while (split)
 	{
 		i++;
 		split = strtok(NULL, SPECIFIER);
 	}
 	free(temp);
-
 	cmd = malloc(sizeof(char *) * (i + 1));
-
 	if(!cmd)
 	{
 		free(line);
 		return (NULL);
 	}
-
-
 		split = strtok(line, SPECIFIER);
 	while (split)
 	{
