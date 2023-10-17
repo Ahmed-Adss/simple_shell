11c181439fca39eb973d3a5dcc42e03733f9455d
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-10-18 02:50:47 +0300

    commit

--- builtin_handle.c
+++ builtin_handle.c
@@ -16,7 +16,7 @@ int builtin_check(char *cmd)
 
 void builtin_handle(char **cmd, char **argv, int *status, int idx)
 {
-	if(_strcmp(cmd[0], "exit") == 0)
+	if (_strcmp(cmd[0], "exit") == 0)
 		shell_exit(cmd, argv, status, idx);
 	else if (_strcmp(cmd[0], "env") == 0)
 		envir_print(cmd, status);
@@ -25,11 +25,11 @@ void builtin_handle(char **cmd, char **argv, int *status, int idx)
 void shell_exit(char **cmd, char **argv, int *status, int idx)
 {
 	int exit_val = (*status);
-	char *index, mssg[] = ": exit: illegal number: ";
+	char *index, mssg[] = ": exit: Illegal number: ";
 
 	if (cmd[1])
 	{
-		if(is_positive_number(cmd[1]))
+		if (is_positive_number(cmd[1]))
 		{
 			exit_val = _atoi(cmd[1]);
 		}
@@ -47,7 +47,7 @@ void shell_exit(char **cmd, char **argv, int *status, int idx)
 			(*status) = 2;
 			return;
 		}
-
+	}
 	free_arr(cmd);
 	exit(exit_val);
 }
